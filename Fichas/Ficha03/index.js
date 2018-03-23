a626686b1ba75ebb872e52b20bb606056a935c80
Temptation77 <jojosd77@gmail.com>
2018-03-23 11:51:12 +0000

    V2.0

--- Fichas/Ficha03/index.js
+++ Fichas/Ficha03/index.js
@@ -1,18 +1,18 @@
 function show(texto) {
-    let meu_texto=document.querySelector("#txtresult")
-    meu_texto.innerHTML=texto
+    let meu_texto = document.querySelector("#txtresult")
+    meu_texto.innerHTML = texto
 }
 //Alínea A 
 function funcA() {
     let string = prompt("Escreva uma string: ")
-    let contador = 0 
+    let contador = 0
     for (let index = 0; index < string.length; index++) {
-       if (string[index] != " "){
-           contador ++
-       }
-        
-    } 
-    show(contador)   
+        if (string[index] != " ") {
+            contador++
+        }
+
+    }
+    show(contador)
 }
 //Alínea B 
 function funcB() {
@@ -23,11 +23,34 @@ function funcB() {
 //Alínea C 
 function funcC() {
     let string = prompt("Escreva aqui: ")
-    let vogais = 0 
+    let vogais = 0
     for (let index = 0; index < string.length; index++) {
-        if (string[index] == "a" ||string[index] == "A" ||string[index] == "e" ||string[index] == "E" ||string[index] == "i" ||string[index] == "I" ||string[index] == "o" ||string[index] == "O" ||string[index] == "u" ||string[index] == "U"){
+        if (string[index] == "a" || string[index] == "A" || string[index] == "e" || string[index] == "E" || string[index] == "i" || string[index] == "I" || string[index] == "o" || string[index] == "O" || string[index] == "u" || string[index] == "U") {
             vogais++
         }
     }
     show(vogais)
 }
+//Alínea D 
+function funcD() {
+    let string = prompt("Escreva aqui: ")
+    let espaços = 0
+    for (let index = 0; index < string.length; index++) {
+        if (string[index] == " ") {
+            espaços++
+        }
+    }
+    show(espaços + 1)
+}
+//Alínea E
+function funcE() {
+    let string = prompt("Escreva aqui: ")
+    let palavra = ""
+    for (let index = string.length-1; index >= 0; index--) {
+        palavra = palavra + string[index]
+    }
+    show(palavra)
+}
+//Alínea F
+
+
