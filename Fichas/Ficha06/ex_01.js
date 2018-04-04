4b80bd48a01c2290011b4a041773098a74f2ae56
Temptation77 <jojosd77@gmail.com>
2018-04-04 13:07:05 +0100

    V1.0

--- Fichas/Ficha06/ex_01.js
+++ Fichas/Ficha06/ex_01.js
@@ -1,20 +1,26 @@
 //Executar o código após o carregamento da página
 window.onload = function() {
-    let myP = document.getElementById("myP")
-    myP.innerHTML = "Bem-vindo à minha página!"
-    let myBtn = document.getElementById("myBtn")
-    //Listener para o botão
-    myBtn.addEventListener("click",function()
-    {
-        let txt = document.getElementById("nome")
-
-        if (txt.value == ""){
-            myP.innerHTML = "Bem-vindo à minha página !"
-        }
-
-        else {
-            myP.innerHTML = "Bem-vindo, " + txt.value
-        }
-    }
-    )
+    init()
 }
+function init() {
+    //Obter as referencias para os elementos HTML
+    let myP = document.getElementById("myP")
+    let myTxt = document.getElementById("myTxt")
+    let myBtn = document.getElementById("myBtn")
+
+    //Alterar o texto do pragrafo 
+    myP.innerHTML = "Bem-vindo à minha página!"
+
+    //Clicar no botão
+    myBtn.addEventListener("click", function() {
+        //Se a caixa de texto não estiver vazia: adiciona no parágrafo o texto que está na caixa de texto
+        if(myTxt.value != "" ){
+            myP.innerHTML = "Bem-vindo " + myTxt.value + "!"
+        }
+         //Se a caixa de texto estiver vazia: mantem o paragrafo original
+         else {
+            myP.innerHTML = "Bem-vindo à minha página!"
+        }
+    })
+}
+
