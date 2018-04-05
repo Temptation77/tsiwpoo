90fbb1e17105ed43b37459befa8fa8eb657b7fdd
Temptation77 <jojosd77@gmail.com>
2018-04-05 10:29:23 +0100

    Sá pinto

--- Fichas/Ficha06/ex_02.js
+++ Fichas/Ficha06/ex_02.js
@@ -1,8 +1,8 @@
-window.onload = function() {
+window.onload = function () {
     init()
 }
 
-function init(){
+function init() {
     //Obtenção das referencias para os elementos HTML
     let btnTodas = document.getElementById("btnTodas")
     let btnCampus1 = document.getElementById("btnCampus1")
@@ -10,39 +10,61 @@ function init(){
     let btnCampus3 = document.getElementById("btnCampus3")
 
     //Clicar no botao TODAS
-    btnTodas.addEventListener("click", function(){
+    btnTodas.addEventListener("click", function () {
+        resetar()
         //Obtem referencia para a coleção de todos os elementos input
         let refInputs = document.getElementsByTagName("input")
         //Itera sobre todos os elementos imput e coloca o seu fundo verde
-        for (let index = 0; index < refInputs.length; index++) {
+        for (let i = 0; i < refInputs.length; i++) {
             refInputs[i].style.backgroundColor = " #00ff00"
         }
     })
     //Clicar no botao CAMPUS I 
-    btnCampus1.addEventListener("click", function(){
+    btnCampus1.addEventListener("click", function () {
+        resetar()
         //Obtem referencia para a coleção de todos os elementos com classe campus1
         let refInputs = document.getElementsByClassName("campus1")
         //Itera sobre todos os elementos imput e coloca o seu fundo verde
-        for (let index = 0; index < refInputs.length; index++) {
+        for (let i = 0; i < refInputs.length; i++) {
             refInputs[i].style.backgroundColor = " #00ff00"
         }
     })
     //Clicar no botao CAMPUS II 
-    btnCampus2.addEventListener("click", function(){
+    btnCampus2.addEventListener("click", function () {
+        resetar()
         //Obtem referencia para a coleção de todos os elementos com classe campus2
         let refInputs = document.getElementsByClassName("campus2")
         //Itera sobre todos os elementos imput e coloca o seu fundo verde
-        for (let index = 0; index < refInputs.length; index++) {
+        for (let i = 0; i < refInputs.length; i++) {
             refInputs[i].style.backgroundColor = " #00ff00"
         }
     })
     //Clicar no botao CAMPUS III
-    btnCampus3.addEventListener("click", function(){
+    btnCampus3.addEventListener("click", function () {
+        resetar()
         //Obtem referencia para a coleção de todos os elementos com classe campus3
         let refInputs = document.getElementsByClassName("campus3")
         //Itera sobre todos os elementos imput e coloca o seu fundo verde
-        for (let index = 0; index < refInputs.length; index++) {
+        for (let i = 0; i < refInputs.length; i++) {
             refInputs[i].style.backgroundColor = "#00ff00"
         }
     })
+}
+
+function resetar() {
+    let campus1 = document.getElementsByClassName("campus1")
+    let campus2 = document.getElementsByClassName("campus2")
+    let campus3 = document.getElementsByClassName("campus3")
+    
+    for (let i = 0; i < campus1.length; i++) {
+        campus1[i].style.backgroundColor = "white" 
+    }
+
+    for (let i = 0; i < campus2.length; i++) {
+        campus2[i].style.backgroundColor = "white" 
+    }
+
+    for (let i = 0; i < campus3.length; i++) {
+        campus3[i].style.backgroundColor = "white" 
+    }
 }
